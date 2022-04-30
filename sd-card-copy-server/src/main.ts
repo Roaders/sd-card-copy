fa87506a80717ff540c2f4fd75831dcfffb9af4d
Giles Roadnight <giles.roadnight@gmail.com>
2022-04-30 09:03:39 +0100

    Implement copy service

--- sd-card-copy-server/src/main.ts
+++ sd-card-copy-server/src/main.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { hostname } from 'os';
 
 async function bootstrap() {
     const port = process.env.port != null ? parseInt(process.env.port) : 3000;
@@ -9,6 +8,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(port);
 
-    console.log(`Application started on ${hostname()}:${port}`);
+    console.log(`Application started on ${port}`);
 }
 bootstrap();
--- sd-card-copy-server/src/services/copy.service.ts
+++ sd-card-copy-server/src/services/copy.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CopyParams } from '../contracts';
-import copyDirectory from 'copydirectory';
+import copyProgress, {
+    configureFileCopyProgressFunction,
+    mapFileStats,
+    streamScanPathResults,
+    isIFilesProgress,
+} from 'copy-progress';
+import { filter, last, map } from 'rxjs';
+import chalk from 'chalk';
 
 @Injectable()
 export class CopyService {
@@ -9,8 +16,21 @@ export class CopyService {
 
         console.log(`Copying ${params.sourcePath} -> ${targetPath}`);
 
-        copyDirectory(params.sourcePath, params.targetPath, () => {
-            console.log(`Copy done`);
-        });
+        const filesStream = streamScanPathResults(params.sourcePath).pipe(
+            map((file) => mapFileStats({ file, outDir: targetPath, sourceDir: params.sourcePath }))
+        );
+
+        copyProgress(filesStream, {
+            concurrentCopy: 1,
+            copyFunction: configureFileCopyProgressFunction({ force: true, highWaterMark: 1024 * 1024 * 1000 }),
+        })
+            .pipe(filter(isIFilesProgress), last())
+            .subscribe({
+                next: (lastValue) =>
+                    console.log(
+                        `Copied ${lastValue.totalFiles} files (${lastValue.totalBytes}b) ${params.sourcePath} -> ${targetPath} in ${lastValue.elapsed}ms`
+                    ),
+                error: (err) => console.log(`${chalk.red('ERR: ')}`, err),
+            });
     }
 }
